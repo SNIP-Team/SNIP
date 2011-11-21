bc6472b510901967a15e7e8af26a8cef849e78e4
ExtremeCoder1337 <schwarz.sebi@gmx.net>
2011-11-21 17:55:05 +0100

    index.php - Script überarbeitet login eingebaut
    styles.css
    - Layout angepasst und in einzelne sektoren eingeteilt
    
    config.php
    - Die DB Daten in eine bestimmte datei gespeichert

---
+++ System/config.php
@@ -0,0 +1,8 @@
+<?php
+  
+  define("Host",""); // Die IP des Hosts wo die Datenbank steht
+  define("MySqlUser",""); // Der Benutzername von der Datenbank
+  define("MySqlPw",""); // Das Passwort von der Datenbank
+  define("DB",""); // Die Datenbank die ausgewählt werden soll
+  
+?>
--- System/index.php
+++ System/index.php
@@ -1,9 +1,46 @@
+<?php
+  
+  session_start();
+  
+  include_once "config.php";
+  
+?>
+
 <html>
   <head>
     <title>SNIP</title>
 	<link rel="stylesheet" type="text/css" href="styles.css" />
   </head>
+  <div id="header">
+    <!-- HEADDER MARKER -->
+	HEAAAAADEEEEER
+  </div>
   <body>
-    <!-- SOURCE MARKER -->
+    <div id="body_left">
+	LEEEEEEEFFFFT
+	</div>
+	<div id="body_main">
+	   <center>
+	   BOOOOOOOOODDDDDDDY MAAAIIIN
+	     <form input="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+	       Username : <input type="text" size="30" name="username" /><br />
+		   Password : &nbsp;<input type="password" size="30" name="password" /><br />
+		   <input type="submit" value="Login !" name="submit" />
+	     </form>
+		 <!--?php
+		   if(isset($_POST("submit")))
+		   {
+		     // hier dann die PHP abfrage aus der DB
+		   }
+		 ?>-->
+	  </center>
+	</div>
+	<div id="body_right">
+	BOOODDYYYYYY RIGHT
+	</div>
   </body>
+  <div id="footer">
+    <!-- FOOTER MARKER -->
+	FOOOOOOTTTEEEER
+  </div>
 </html>
