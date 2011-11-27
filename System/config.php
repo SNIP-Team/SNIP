a81ef76bd813f0091efcbb7fe3803139f58d3233
ExtremeCoder1337 <schwarz.sebi@gmx.net>
2011-11-27 16:38:53 +0100

    Daten Versteckt : )

--- System/config.php
+++ System/config.php
@@ -1,8 +1,8 @@
 <?php
   
-  define("Host","127.0.0.1"); // Die IP des Hosts wo die Datenbank steht
-  define("MySqlUser","mes_snip"); // Der Benutzername von der Datenbank
-  define("MySqlPw","qaywsxedc"); // Das Passwort von der Datenbank
-  define("DB","mes_snip"); // Die Datenbank die ausgewählt werden soll
+  define("Host","******"); // Die IP des Hosts wo die Datenbank steht
+  define("MySqlUser","******"); // Der Benutzername von der Datenbank
+  define("MySqlPw","*****"); // Das Passwort von der Datenbank
+  define("DB","******"); // Die Datenbank die ausgewählt werden soll
   
 ?>
